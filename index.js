2f21b4b93c85a75d9d42c45961858216bc612fc8
Ymt <tuanit2304@gmail.com>
2020-02-12 14:25:01 +0700

    remove cat eat dog

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var Mouse = require('./Mouse');
 var dog = new Dog('Chow');
 var cat = new Cat();
 var mouse = new Mouse('Mickey');
-//cat.eat(mouse);
-try {
-	cat.eat(dog);
-} catch (error) {
-	console.log('Error while cat eating a dog');
-}
+cat.eat(mouse);
+// try {
+// 	cat.eat(dog);
+// } catch (error) {
+// 	console.log('Error while cat eating a dog');
+// }
 console.log(cat);
 dog.sayHi();
