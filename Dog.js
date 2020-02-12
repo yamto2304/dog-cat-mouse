88e4a328547b5556b7c993ae82d38a64fff56d37
Ymt <tuanit2304@gmail.com>
2020-02-12 14:20:52 +0700

    test some thing interesting !

--- Dog.js
+++ Dog.js
@@ -1,16 +1,23 @@
 var chalk = require('chalk');
+var Cat = require('./Cat');
+
+var cat = new Cat();
 
 function Dog(name) {
 	this.name = name;
 	this.stomach= [];
 }
 
-Dog.prototype.eat = function(Cat) {
-	this.stomach.push(Cat);	
+var dog = new Dog();
+
+Dog.prototype.eat = function(animal) {
+	this.stomach.push(animal);	
 };
 
 Dog.prototype.sayHi = function(){
 	console.log('MeoMeo ' + chalk.red(this.name) + '!!');
+	dog.eat(cat);
+	console.log(dog);
 };
 
 module.exports = Dog;
